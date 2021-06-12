28715bf047bdb3967b556bb13d6070e965995432
Dillon Baxendell <dillon.j.baxendell@gmail.com>
2021-06-11 20:26:56 -0500

    Admin html skeleton created

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,33 @@
+//Import CSS
+import "./Admin.css";
+
+//FUNCTION Admin
+function Admin () {
+    return (
+        <>
+            <h1>Feedback Results</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Feeling</th>
+                        <th>Comprehension</th>
+                        <th>Support</th>
+                        <th>Comments</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+
+
+
+            </table>
+
+
+        </>
+    )
+}
+
+//EXPORT this function
+export default Admin;
